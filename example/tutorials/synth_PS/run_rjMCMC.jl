66f719b37f3f4029566b911e765ec5f15b95fa03
gianmarcodelpiccolo <102164928+gianmarcodelpiccolo@users.noreply.github.com>
2024-09-14 23:47:05 -0700

    Delete example/tutorials/synth_PS directory

--- example/tutorials/synth_PS/run_rjMCMC.jl
+++
@@ -1,9 +0,0 @@
-using Distributed
-addprocs(parse(Int64, ARGS[2]))
-@everywhere include(ENV["PSI_S"]*"/src/dependencies.jl")
-
-const wrk_dir = @__DIR__
-const name = string(ARGS[1])
-const chain_id = parse(Int64,string(ARGS[3]))
-run_RJMCMC(wrk_dir, name, chain_id)
-
